1c212962bd878af9ccf20fae099939b22e4b2b15
OpanDilaga <51019063+OpanDilaga@users.noreply.github.com>
2019-05-29 13:26:01 +0700

    java
    upload java

---
+++ PBOJ&S-A2.1700084-Latihan53/src/latihan53/Animal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan53;
+
+/**
+ *
+ * @author acer
+ * Nama     : Opan Dilaga
+ * NIM      : A2.1700084
+ * Kelas    : TI IV E J&S
+ */
+public class Animal {
+    protected boolean vegetarian; 
+    protected String eats; 
+    protected int no0fLegs;
+    
+    
+    public Animal(boolean vegetarian, String food, int legs){ 
+            this.vegetarian = vegetarian; 
+            this.eats = food; 
+            this.no0fLegs = legs;
+} 
+    public boolean isVegetarian() {
+            return vegetarian; 
+    } 
+    public String getEats() {
+            return eats; 
+    }
+    
+    public int getNoOfLegs(){
+        int noOfLegs = 4;
+        return noOfLegs;
+    }
+        
+}
---
+++ PBOJ&S-A2.1700084-Latihan53/src/latihan53/Rabbit.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan53;
+
+/**
+ *
+ * @author acer
+ * Nama     : Opan Dilaga
+ * NIM      : A2.1700084
+ * Kelas    : TI IV E J&S
+ */
+public class Rabbit extends Animal {
+     private String color; 
+     private String name; 
+     public Rabbit (String name, boolean veg, String food, int legs, String color ){
+         super (veg, food, legs);
+         this.name = name;
+         this.color = color;
+     
+     }
+     
+     public String getColor(){
+         return color;
+     }
+     
+     public String getName(){
+         return name;
+     }
+
+}
